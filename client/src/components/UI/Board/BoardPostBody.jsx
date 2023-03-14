17088d37e59e9aa19cb241d9053eff5f397b4192
dotorilover <doriddori_@naver.com>
2023-03-14 17:33:59 +0900

    design:BoardPostForm 작업

---
+++ client/src/components/UI/Board/BoardPostBody.jsx
@@ -0,0 +1,11 @@
+import TextInput from '../TextInput';
+
+function BoardPostBody() {
+  return (
+    <div className="w-full h-5 mx-4 mt-6 text-xl">
+      <TextInput placeholder="내용을 입력하세요." />
+    </div>
+  );
+}
+
+export default BoardPostBody;
--- client/src/components/UI/TabButton.jsx
+++ client/src/components/UI/TabButton.jsx
@@ -1,6 +1,6 @@
 function TabButton({ text }) {
   return (
-    <li className="border border-[#575757] rounded-full w-[60px] flex justify-center mr-2">
+    <li className="border border-[#575757] rounded-full px-2  flex justify-center mr-2">
       <span className="text-[#575757]">{text}</span>
     </li>
   );
