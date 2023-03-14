59e34b655e558cccd3bab07bbb288a8ff7d480f0
youjin <uzn.biz@gmail.com>
2023-03-14 14:43:21 +0900

    Test: 프로젝트 배포 실행 설정 수정

---
+++ back/src/main/java/main012/server/HelloController.java
@@ -0,0 +1,13 @@
+package main012.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String Hello () {
+        return "hello world";
+    }
+}
