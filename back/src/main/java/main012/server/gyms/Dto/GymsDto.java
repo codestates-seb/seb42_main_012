8cf3a2d07baf00b40211404aa51c04111cf7f4c0
kim <whdrb335@gmail.com>
2023-03-15 10:55:53 +0900

    Test: 뉴 브랜치 생성 테스트

---
+++ back/src/main/java/main012/server/gyms/Dto/GymsDto.java
@@ -0,0 +1,30 @@
+package main012.server.gyms.Dto;
+
+public class GymsDto {
+    private String gymName;
+    private String address;
+    private String offDays;
+    private String facilities;
+    private long phoneNumber;
+    private long prices;
+    private long openingTime;
+
+    public String getgymName() {
+        return gymName;
+    }
+
+    public void setGymName(String gymName) {
+        this.gymName = gymName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress() {
+        this.address = address;
+    }
+
+
+
+}
---
+++ back/src/main/java/main012/server/gyms/Entity/GymsEntity.java
@@ -0,0 +1,5 @@
+package main012.server.gyms.Entity;
+
+
+public class GymsEntity {
+}
