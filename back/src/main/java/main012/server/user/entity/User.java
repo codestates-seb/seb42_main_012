eb4f85663a257d02cdd8d6e315ff3fea98a3bf19
jjeongeunee <jjeongee99@gmail.com>
2023-03-15 14:35:50 +0900

    Style: user_role 테이블 이름 수정

--- back/src/main/java/main012/server/user/entity/User.java
+++ back/src/main/java/main012/server/user/entity/User.java
@@ -34,7 +34,7 @@ public class User extends Auditable {
     // N : N
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
-            name = "users_role",
+            name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
