a02f483b2cbe7b4e6ab162f368658d8b521e00e6
jjeongeunee <jjeongee99@gmail.com>
2023-03-16 13:07:14 +0900

    Remove: 에러나는 부분 삭제

--- back/src/main/java/main012/server/gym/controller/GymController.java
+++ back/src/main/java/main012/server/gym/controller/GymController.java
@@ -19,58 +19,5 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/gyms")
 public class GymController {
-    private final GymService gymService;
-    private final GymMapper mapper;
 
-    public GymController(GymService gymService, GymMapper mapper) {
-        this.gymService = gymService;
-        this.mapper = mapper;
-    }
-    // 헬스장 등록
-    // 여기에 나머지 폰넘버랑 다른거 작성해야함
-    @PostMapping
-    public ResponseEntity postGym(@Valid @RequestBody GymPostDto gymPostDto) {
-        Gym gym = mapper.gymPostDtoToGym(gymPostDto);
-
-        Gym response = gymService.createGym(gym);
-
-        return new ResponseEntity<>(mapper.gymToGymResponseDto(response), HttpStatus.CREATED);
-    }
-
-    // 헬스장 정보 수정
-    @PatchMapping("/gym_id")
-    public ResponseEntity patchGym(@PathVariable("gym_id") @Min(1) long gymId,
-                                   @Valid @RequestBody GymPatchDto gymPatchDto){
-        gymPatchDto.setGymId(gymId);
-
-        Gym response = gymService.updateGym(mapper.gymPatchDtoToGym(gymPatchDto));
-        return new ResponseEntity<>(mapper.gymToGymResponseDto(response), HttpStatus.OK);
-    }
-
-    // 헬스장 상세조회
-    @GetMapping("/{gym_id}")
-    public ResponseEntity getGym(@PathVariable("gym_id") @Positive long gymId) {
-        Gym response = gymService.findGym(gymId);
-
-        return new ResponseEntity<>(mapper.gymToGymResponseDto(response),HttpStatus.OK);
-    }
-
-    // 헬스장 전체조회
-    @GetMapping
-    public ResponseEntity getGyms() {
-        List<Gym> gyms = gymService.findGyms();
-        List<GymResponseDto> response =
-                gyms.stream().map(gym -> mapper.gymToGymResponseDto(gym))
-                        .collect(Collectors.toList());
-
-        return new ResponseEntity<>(response,HttpStatus.OK);
-    }
-
-    // 헬스장 정보 삭제
-    @DeleteMapping("/{gym_id}")
-    public ResponseEntity deleteGym(@PathVariable("gym_id") long gymId){
-        System.out.println("# delete Gym");
-        gymService.deleteGym(gymId);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
 }
--- back/src/main/java/main012/server/gym/mapper/GymMapper.java
+++ back/src/main/java/main012/server/gym/mapper/GymMapper.java
@@ -1,14 +1,8 @@
 package main012.server.gym.mapper;
 
-import main012.server.gym.dto.GymPatchDto;
-import main012.server.gym.dto.GymPostDto;
-import main012.server.gym.dto.GymResponseDto;
-import main012.server.gym.entity.Gym;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface GymMapper {
-    Gym gymPostDtoToGym(GymPostDto gymPostDto);
-    Gym gymPatchDtoToGym(GymPatchDto gymPatchDto);
-    GymResponseDto gymToGymResponseDto(Gym gym);
+
 }
