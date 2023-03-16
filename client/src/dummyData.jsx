a824b47de29ca9a214744c47144eff1919556da0
dotorilover <doriddori_@naver.com>
2023-03-16 23:38:47 +0900

    comment:dummydata 생성

---
+++ client/src/dummyData.jsx
@@ -0,0 +1,729 @@
+export const dummyComment = {
+  data: [
+    {
+      comment: '감사합니다 좋은 정보 알아가네요 호호',
+      memberId: 1,
+    },
+    {
+      comment: '퍼가요 ^&^',
+      memberId: 2,
+    },
+    {
+      comment: '안녕하세요 오늘도 득근 하세요!',
+      memberId: 3,
+    },
+    {
+      comment:
+        '바른 자세는 척추가 정상적인 곡선을 이루고 있고 하반신의 골격이 체중을 유지할 수 있는 이상적인 정렬상태를 말한다. 중립자세에 있는 골반은 복부와 몸통, 그리고 그 아래에 놓인 하지가 올바른 정렬이 되도록 돕고 가슴과 등 상부는 호흡기가 활동하기 좋은 상태를 만들어 준다. 또한 머리는 균형이 잘 잡힌 상태에서 목 근육의 스트레스를 최소화시킨다. ',
+      memberId: 4,
+    },
+    {
+      comment:
+        '바르지 못한 목의 자세는 목이 앞으로 빠져나와 있고 등은 굽어 있는 상태이다. 이런 자세가 오래 지속되게 되면 목뒤 근육이 비대해져 낙타등과 같이 불룩해진다. ',
+      memberId: 5,
+    },
+    {
+      comment: '감사합니다 좋은 정보 알아가네요 호호',
+      memberId: 1,
+    },
+    {
+      comment: '퍼가요 ^&^',
+      memberId: 2,
+    },
+    {
+      comment: '안녕하세요 오늘도 득근 하세요!',
+      memberId: 3,
+    },
+    {
+      comment:
+        '바른 자세는 척추가 정상적인 곡선을 이루고 있고 하반신의 골격이 체중을 유지할 수 있는 이상적인 정렬상태를 말한다. 중립자세에 있는 골반은 복부와 몸통, 그리고 그 아래에 놓인 하지가 올바른 정렬이 되도록 돕고 가슴과 등 상부는 호흡기가 활동하기 좋은 상태를 만들어 준다. 또한 머리는 균형이 잘 잡힌 상태에서 목 근육의 스트레스를 최소화시킨다. ',
+      memberId: 4,
+    },
+    {
+      comment:
+        '바르지 못한 목의 자세는 목이 앞으로 빠져나와 있고 등은 굽어 있는 상태이다. 이런 자세가 오래 지속되게 되면 목뒤 근육이 비대해져 낙타등과 같이 불룩해진다. ',
+      memberId: 5,
+    },
+  ],
+};
+
+export const dummyBoard = {
+  data: [
+    {
+      data: {
+        contents: [
+          {
+            boardId: 1,
+            title: '운동 잘하는 법 공유',
+            tabName: '자유게시판',
+            createdAt: '2023-12-04',
+            views: 124,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 2,
+            title: '날씬해지는 식단 궁금하면 드루와',
+            tabName: '자유게시판',
+            createdAt: '2023-12-04',
+            views: 1,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 3,
+            title: '운동 루틴 추천해주세요',
+            tabName: '자유게시판',
+            createdAt: '2023-12-04',
+            views: 30,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 4,
+            title: '자세 이거 맞나요?',
+            tabName: '자세피드백',
+            createdAt: '2023-12-04',
+            views: 9,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 5,
+            title: '헬린이 여기 헬스장 추천좀여',
+            tabName: '자유게시판',
+            createdAt: '2023-12-04',
+            views: 3,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 6,
+            title: '헬스장 고를때 꿀팁',
+            tabName: '꿀팁',
+            createdAt: '2023-12-04',
+            views: 290,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 7,
+            title: '하체 루틴 짜주세요',
+            tabName: '운동루틴',
+            createdAt: '2023-12-05',
+            views: 2,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 8,
+            title: '여름맞이 다이어트 같이해요',
+            tabName: '파트너모집',
+            createdAt: '2023-12-05',
+            views: 445,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 9,
+            title: '운동 가기 싫다',
+            tabName: '자유게시판',
+            createdAt: '2023-12-06',
+            views: 20,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            boardId: 10,
+            title: '야밤에 치킨 먹기',
+            tabName: '자유게시판',
+            createdAt: '2023-12-07',
+            views: 40,
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+  ],
+};
+
+export const dummyBoardDetail = {
+  data: [
+    {
+      ProfileImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      displayName: 'dotori',
+      tabName: '자유게시판',
+      title: '오늘 헬스장 가기 싫다',
+      content: '가기 싫다',
+      contentImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      createdAt: '2023-04-10',
+      boardBookmarkCnt: '9',
+    },
+    {
+      ProfileImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      displayName: '냥냥이',
+      tabName: '오운완인증',
+      title: 'title',
+      content: 'content',
+      contentImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      createdAt: '2023-04-10',
+      boardBookmarkCnt: '0',
+    },
+    {
+      ProfileImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      displayName: '갱얼즤2',
+      tabName: '파트너모집',
+      title: 'title',
+      content: 'content',
+      contentImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      createdAt: '2023-04-10',
+      boardBookmarkCnt: '0',
+    },
+    {
+      ProfileImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      displayName: '도토리',
+      tabName: '운동루틴',
+      title: 'title',
+      content: 'content',
+      contentImage:
+        'https://image.shutterstock.com/image-vector/modern-flat-character-woman-dream-260nw-1520037971.jpg',
+      createdAt: '2023-04-10',
+      boardBookmarkCnt: '0',
+    },
+  ],
+};
+export const dummyReviews = {
+  data: [
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 1,
+            displayName: '도토리',
+            comment:
+              '음...여기 너무 깨끗하고 넓고 쾌적해요, 값짒맚셊욦 샤워실 최고 덟럽웝룝',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 2,
+            displayName: '양갱이',
+            comment: '기구 종류가 다양해서 좋아요!!',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 3,
+            displayName: '김자반',
+            comment: '트레이너쌤들 친철하세요 ㅜㅜ ',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 4,
+            displayName: '김코딩',
+            comment: '쾌적합니다.',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 5,
+            displayName: '박해커',
+            comment: '두번 가고싶지는 않음',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 6,
+            displayName: '간장공장공장장',
+            comment: '다른 헬스장에 비해 비싼편 인것같슴니다,..',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 7,
+            displayName: '퇴사하고싶다',
+            comment: '사람은 많지만 가성비 있게 다닐 수 있어서 좋네요',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 8,
+            displayName: '잼민이',
+            comment: '퇴근시간인데도 한가해서 좋아요 ',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 9,
+            displayName: '학생1',
+            comment: '부대시설 모두 잘되어있네요',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            reviewId: 10,
+            displayName: '학생2',
+            comment: '좋습니다',
+            CreatedAt: '2023-01-01',
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+  ],
+};
+export const dummyGymList = {
+  data: [
+    {
+      data: {
+        contents: [
+          {
+            gymId: 1,
+            gymImage:
+              'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+            gymName: '건강해짐',
+            address: 'test',
+            prices: [
+              {
+                priceId: 1,
+                priceName: '3달 등록비',
+                price: 100000,
+              },
+              {
+                priceId: 2,
+                priceName: '10회 pt 비용',
+                price: 1000000,
+              },
+              {
+                priceId: 3,
+                priceName: '락커 및 운동복 대여',
+                price: 10000,
+              },
+            ],
+            reviewAverage: 4.5,
+            facilities: [
+              {
+                facilityId: 1,
+                facilityName: '운동복',
+              },
+              {
+                facilityId: 4,
+                facilityName: '락커',
+              },
+            ],
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            gymId: 2,
+            gymImage:
+              'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+            gymName: '건강건강해짐',
+            address: 'test',
+            prices: [
+              {
+                priceId: 1,
+                priceName: '3달 등록비',
+                price: 100000,
+              },
+              {
+                priceId: 2,
+                priceName: '10회 pt 비용',
+                price: 1000000,
+              },
+              {
+                priceId: 3,
+                priceName: '락커 및 운동복 대여',
+                price: 10000,
+              },
+            ],
+            reviewAverage: 5,
+            facilities: [
+              {
+                facilityId: 1,
+                facilityName: '운동복',
+              },
+              {
+                facilityId: 4,
+                facilityName: '락커',
+              },
+            ],
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            gymId: 3,
+            gymImage:
+              'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+            gymName: '서울휘트니스',
+            address: 'test',
+            prices: [
+              {
+                priceId: 1,
+                priceName: '3달 등록비',
+                price: 100000,
+              },
+              {
+                priceId: 2,
+                priceName: '10회 pt 비용',
+                price: 1000000,
+              },
+              {
+                priceId: 3,
+                priceName: '락커 및 운동복 대여',
+                price: 10000,
+              },
+            ],
+            reviewAverage: 3,
+            facilities: [
+              {
+                facilityId: 1,
+                facilityName: '운동복',
+              },
+              {
+                facilityId: 4,
+                facilityName: '락커',
+              },
+            ],
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            gymId: 4,
+            gymImage:
+              'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+            gymName: '어몽짐',
+            address: 'test',
+            prices: [
+              {
+                priceId: 1,
+                priceName: '3달 등록비',
+                price: 100000,
+              },
+              {
+                priceId: 2,
+                priceName: '10회 pt 비용',
+                price: 1000000,
+              },
+              {
+                priceId: 3,
+                priceName: '락커 및 운동복 대여',
+                price: 10000,
+              },
+            ],
+            reviewAverage: 4.2,
+            facilities: [
+              {
+                facilityId: 1,
+                facilityName: '운동복',
+              },
+              {
+                facilityId: 4,
+                facilityName: '락커',
+              },
+            ],
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+    {
+      data: {
+        contents: [
+          {
+            gymId: 5,
+            gymImage:
+              'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+            gymName: '짐핏휘트니스',
+            address: 'test',
+            prices: [
+              {
+                priceId: 1,
+                priceName: '3달 등록비',
+                price: 100000,
+              },
+              {
+                priceId: 2,
+                priceName: '10회 pt 비용',
+                price: 1000000,
+              },
+              {
+                priceId: 3,
+                priceName: '락커 및 운동복 대여',
+                price: 10000,
+              },
+            ],
+            reviewAverage: 2.8,
+            facilities: [
+              {
+                facilityId: 1,
+                facilityName: '운동복',
+              },
+              {
+                facilityId: 4,
+                facilityName: '락커',
+              },
+            ],
+          },
+        ],
+        totalElements: 20,
+        nextCursor: 20,
+      },
+    },
+  ],
+};
+export const dummyGymDetailList = {
+  data: [
+    {
+      gymName: '건강해짐',
+      gymImage:
+        'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+      address: '서울',
+      phoneNumber: '01012345678',
+      prices: '140,000',
+      // "gymImage" : "url"
+      businessHours: '09:00 ~ 23:00 / 휴무일 : 월요일 ',
+      facilities: [
+        {
+          facilityId: 1,
+          facilityName: '운동복',
+        },
+        {
+          facilityId: 4,
+          facilityName: '락커',
+        },
+      ],
+    },
+    {
+      gymName: '어몽휘트니스',
+      gymImage:
+        'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+      address: '서울',
+      phoneNumber: '01012345678',
+      prices: '30,000',
+      // "gymImage" : "url"
+      businessHours: '09:00 ~ 23:00 / 휴무일 : 월요일 ',
+      facilities: [
+        {
+          facilityId: 1,
+          facilityName: '운동복',
+        },
+        {
+          facilityId: 4,
+          facilityName: '락커',
+        },
+      ],
+    },
+    {
+      gymName: '정글짐',
+      gymImage:
+        'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+      address: '서울',
+      phoneNumber: '01012345678',
+      prices: '240,000',
+      // "gymImage" : "url"
+      businessHours: '09:00 ~ 23:00 / 휴무일 : 월요일 ',
+      facilities: [
+        {
+          facilityId: 1,
+          facilityName: '운동복',
+        },
+        {
+          facilityId: 4,
+          facilityName: '락커',
+        },
+      ],
+    },
+    {
+      gymName: '베스트짐',
+      gymImage:
+        'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+      address: '서울',
+      phoneNumber: '01012345678',
+      prices: '180,000',
+      // "gymImage" : "url"
+      businessHours: '09:00 ~ 23:00 / 휴무일 : 월요일 ',
+      facilities: [
+        {
+          facilityId: 1,
+          facilityName: '운동복',
+        },
+        {
+          facilityId: 4,
+          facilityName: '락커',
+        },
+      ],
+    },
+    {
+      gymName: '썸머휘트니스',
+      gymImage:
+        'https://mediahub.seoul.go.kr/uploads/mediahub/2021/01/d617bd125f644bae918def98044087aa.jpg',
+      address: '서울',
+      phoneNumber: '01012345678',
+      prices: '200,000',
+      // "gymImage" : "url"
+      businessHours: '09:00 ~ 23:00 / 휴무일 : 월요일 ',
+      facilities: [
+        {
+          facilityId: 1,
+          facilityName: '운동복',
+        },
+        {
+          facilityId: 4,
+          facilityName: '락커',
+        },
+      ],
+    },
+  ],
+};
+// export const dummyCommunites = {
+//   data: [
+
+//   ],
+// };
