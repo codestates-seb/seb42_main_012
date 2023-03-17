56c144f074f36bb42924e33c4dbfccebc9a533dc
kimyonghui <kyhui1115@gmail.com>
2023-03-17 19:20:08 +0900

    chore: 더미데이터 수정

--- client/src/components/Gym/Hours/HoursContainer.jsx
+++ client/src/components/Gym/Hours/HoursContainer.jsx
@@ -1,22 +1,7 @@
 function GymHoursContainer() {
-  return (
-    <p className="px-4 space-y-2 text-sm">
-      {/* useState */}
-      월요일 : 정기휴무
-      <br />
-      화요일 : 07:00 ~ 23:30
-      <br />
-      수요일 : 07:00 ~ 23:30
-      <br />
-      목요일 : 07:00 ~ 23:30
-      <br />
-      금요일 : 07:00 ~ 23:30
-      <br />
-      토요일 : 07:00 ~ 23:30
-      <br />
-      일요일 : 10:00 ~ 19:00
-    </p>
-  );
+  const text = `월요일 : 정기휴무\n화요일 : 07:00 ~ 23:30\n수요일 : 07:00 ~ 23:30\n목요일 : 07:00 ~ 23:30\n금요일 : 07:00 ~ 23:30\n토요일 : 07:00 ~ 23:30\n일요일 : 10:00 ~ 19:00`;
+
+  return <p className="px-4 space-y-2 text-sm whitespace-pre-wrap">{text}</p>;
 }
 
 export default GymHoursContainer;
--- client/src/components/Gym/Price/PriceTableContainer.jsx
+++ client/src/components/Gym/Price/PriceTableContainer.jsx
@@ -1,57 +1,9 @@
 // import GymPriceTable from './GymPriceTable';
 
 function GymPriceTableContainer() {
-  // const prices = [
-  //   {
-  //     priceName: 1,
-  //     price: '60,000원',
-  //   },
-  //   {
-  //     priceName: 3,
-  //     price: '150,000원',
-  //   },
-  //   {
-  //     priceName: 6,
-  //     price: '250,000원',
-  //   },
-  // ];
+  const text = `[헬스장이용권]\n 1개월 : 60,000원\n 3개월 : 150,000원\n 6개월 : 250,000원\n\n[PT]\n 10회 : 540,000원\n 10회 : 960,000원\n 10회 : 1,050,000원`;
 
-  // const prices2 = [
-  //   {
-  //     priceName: 10,
-  //     price: '540,000원',
-  //   },
-  //   {
-  //     priceName: 20,
-  //     price: '960,000원',
-  //   },
-  //   {
-  //     priceName: 30,
-  //     price: '1,050,000원',
-  //   },
-  // ];
-
-  return (
-    <p className="px-4 space-y-2 text-sm">
-      [헬스장이용권]
-      <br />
-      1개월 : 60,000원
-      <br />
-      3개월 : 150,000원
-      <br />
-      6개월 : 250,000원
-      <br />
-      <br />
-      [PT]
-      <br />
-      10회 : 540,000원
-      <br />
-      10회 : 960,000원
-      <br />
-      10회 : 1,050,000원
-      <br />
-    </p>
-  );
+  return <p className="px-4 space-y-2 text-sm whitespace-pre-wrap">{text}</p>;
 }
 
 export default GymPriceTableContainer;
