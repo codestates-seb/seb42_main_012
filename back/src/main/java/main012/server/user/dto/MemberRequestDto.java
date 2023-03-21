cb8957ad47367293fca56c02e5910aa38a748e62
jjeongeunee <jjeongee99@gmail.com>
2023-03-21 16:41:00 +0900

    Fix: validation 수정

--- back/src/main/java/main012/server/user/dto/MemberRequestDto.java
+++ back/src/main/java/main012/server/user/dto/MemberRequestDto.java
@@ -21,8 +21,6 @@ public class MemberRequestDto {
         @NotBlank(message = "닉네임은 필수 입력값입니다.")
         private String displayName;
 
-        @Pattern(regexp = "^(?=.*[A-Za-z!@#$%^&*])(?=.*\\d)[A-Za-z!@#$%^&*\\d]{8,}$",
-                message = "비밀번호는 최소 4자 이상, 숫자와 영문자의 조합으로 이루어져야 합니다. (특수문자 추가 가능)")
         private String password;
     }
 
@@ -39,8 +37,6 @@ public class MemberRequestDto {
 
         private String password;
 
-        @Pattern(regexp = "",
-                message = "사업자등록번호는 9자의 숫자로 입력되어야 합니다.")
         private String businessNumber;
     }
 
