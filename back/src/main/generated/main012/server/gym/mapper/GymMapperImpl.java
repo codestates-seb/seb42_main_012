aa130b855fe384edb757b49d508fd5e7a934aa5a
kim <whdrb335@gmail.com>
2023-03-22 18:55:06 +0900

    Feat: 수정

--- back/src/main/generated/main012/server/gym/mapper/GymMapperImpl.java
+++ back/src/main/generated/main012/server/gym/mapper/GymMapperImpl.java
@@ -8,11 +8,6 @@ import main012.server.gym.dto.GymResponseDto;
 import main012.server.gym.entity.Gym;
 import org.springframework.stereotype.Component;
 
-@Generated(
-    value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-22T18:42:05+0900",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
-)
 @Component
 public class GymMapperImpl implements GymMapper {
 
