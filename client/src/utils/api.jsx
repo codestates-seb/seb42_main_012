05760d1d4e15ff49a5c43530c872ad176d99ddfb
kimyonghui <kyhui1115@gmail.com>
2023-03-23 22:59:37 +0900

    feat: 엑시오스 인스턴스 수정

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://ec2-13-124-61-156.ap-northeast-2.compute.amazonaws.com:8080',
+  retryCount: 3,
 });
 
 api.interceptors.response.use(
@@ -18,7 +19,9 @@ api.interceptors.response.use(
   },
 
   async error => {
+    const originalRequest = error.config;
     if (error.response.status === 401) {
+      console.log('토큰 만료');
       const data = await api.post('/auth/refresh', {
         headers: {
           authorization: localStorage.getItem('refreshToken'),
@@ -31,7 +34,7 @@ api.interceptors.response.use(
         data.headers['authorization-refresh'],
       );
 
-      return api(data.config);
+      return api(originalRequest);
     }
     return Promise.reject(error);
   },
