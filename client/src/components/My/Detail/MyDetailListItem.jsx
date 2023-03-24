4eb4654e82303c0ac9ae41b42558899c9abe4cec
mya413 <lsm940413@gmail.com>
2023-03-25 01:50:07 +0900

    feat: 마이페이지 구현중

--- client/src/components/My/Detail/MyDetailListItem.jsx
+++ client/src/components/My/Detail/MyDetailListItem.jsx
@@ -1,50 +1,24 @@
 import { useLocation } from 'react-router-dom';
 import ReviewScore from '../../Gym/Review/ReviewScore';
 import TabButton from '../../UI/Button/TabButton';
-import useStore from '../../../state/useStore';
 
-function MyDetailListItem({ title, created }) {
+function MyDetailListItem({ tabName, title, created, grades, image }) {
   const location = useLocation();
-  const {
-    myBoards,
-    myComments,
-    myReviews,
-    myGymsBookmarks,
-    myBoardsBookmarks,
-  } = useStore();
 
   return (
     <li className="flex items-center justify-between px-4 py-8 border-y border-[var(--main-border)] active:bg-[var(--main-active)]">
       <div className="flex items-center justify-center">
-        {location.pathname === '/my/board'
-          ? myBoards.contents.map(board => (
-              <TabButton key={board.boardId} tabName={board.boardTab[0]} />
-            ))
-          : location.pathname === '/my/comments'
-          ? myComments.contents.map(comment => (
-              <TabButton key={comment.boardId} tabName={comment.boardTab} />
-            ))
-          : location.pathname === '/my/reviews'
-          ? myReviews.contents.map(review => (
-              <ReviewScore key={review.gymId} grades={review.gymGrade} />
-            ))
-          : location.pathname === '/my/bookmarks/gyms'
-          ? myGymsBookmarks.contents.map(gymBookmark => (
-              <img
-                className="w-40"
-                src={gymBookmark.gymImage[0]}
-                alt="헬스장이미지"
-                key={gymBookmark.gymId}
-              />
-            ))
-          : location.pathname === '/my/bookmarks/board'
-          ? myBoardsBookmarks.contents.map(boardBookmark => (
-              <TabButton
-                key={boardBookmark.boardId}
-                tabName={boardBookmark.boardTab}
-              />
-            ))
-          : null}
+        {location.pathname === '/my/board' ? (
+          <TabButton tabName={tabName} />
+        ) : location.pathname === '/my/comments' ? (
+          <TabButton tabName={tabName} />
+        ) : location.pathname === '/my/reviews' ? (
+          <ReviewScore grades={grades} />
+        ) : location.pathname === '/my/bookmarks/gyms' ? (
+          <img className="w-40" src={image} alt="헬스장이미지" />
+        ) : location.pathname === '/my/bookmarks/board' ? (
+          <TabButton tabName={tabName} />
+        ) : null}
         <h3 className="ml-2 line-clamp-1">{title}</h3>
       </div>
       <span className="text-[var(--second)]">{created}</span>
--- client/src/components/My/Profile.jsx
+++ client/src/components/My/Profile.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import ProfileImg from '../UI/ProfileImg/ProfileImg';
-import DisplayName from '../UI/DisplayName/DisplayName';
 import EditButton from '../UI/Button/EditButton';
-import useStore from '../../state/useStore';
+// import useStore from '../../state/useStore';
 
 function Profile() {
-  const { myElements } = useStore();
-  const [profileImage, setProfileImageUrl] = useState(null);
-
   return (
     <div className="flex items-center">
       <div className="relative">
-        {myElements && (
-          <ProfileImg
-            profileImage={profileImage}
-            setProfileImageUrl={setProfileImageUrl}
-            page="my"
-          />
-        )}
+        <ProfileImg page="my" />
       </div>
-
-      <DisplayName displayName={myElements.displayName} />
-
-      <input type="text" name="displayName" />
       <EditButton />
     </div>
   );
