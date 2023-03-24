bd07026d3691d8d4290be01153ee370170d43e17
mya413 <lsm940413@gmail.com>
2023-03-24 17:29:33 +0900

    Chore: 유효성검사 추가

--- client/src/components/UI/Button/MyPageButton.jsx
+++ client/src/components/UI/Button/MyPageButton.jsx
@@ -1,14 +1,59 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import useStore from '../../../state/useStore';
+import api from '../../../utils/api';
 
 function MyPageButton({ to, text }) {
   const {
+    setMyBoards,
+    setMyComments,
+    setMyReviews,
+    setMyGymsBookmarks,
+    setBoardsBookmarks,
     myBoards,
     myComments,
     myReviews,
     myGymsBookmarks,
     myBoardsBookmarks,
   } = useStore();
+
+  console.log(
+    myBoards,
+    myComments,
+    myReviews,
+    myGymsBookmarks,
+    myBoardsBookmarks,
+  );
+
+  useEffect(() => {
+    api
+      .get('/members/my/communities?lastFeedId=56')
+      .then(res => setMyBoards(res.data.data));
+  }, []);
+
+  useEffect(() => {
+    api
+      .get('/members/my/comments?lastFeedId=15')
+      .then(res => setMyComments(res.data.data));
+  }, []);
+
+  useEffect(() => {
+    api
+      .get('/members/my/reviews?lastFeedId=15')
+      .then(res => setMyReviews(res.data.data));
+  }, []);
+
+  useEffect(() => {
+    api
+      .get('/members/my/bookmarks/gyms?lastFeedId=15')
+      .then(res => setMyGymsBookmarks(res.data.data));
+  }, []);
+
+  useEffect(() => {
+    api
+      .get('/members/my/bookmarks/communities?lastFeedId=15')
+      .then(res => setBoardsBookmarks(res.data.data));
+  }, []);
 
   return (
     <li className="z-10 flex-auto w-1/3">
@@ -28,9 +73,9 @@ function MyPageButton({ to, text }) {
           <span className="text-2xl font-bold">
             {myBoardsBookmarks.totalCnt}
           </span>
-        ) : (
+        ) : to === null || to === undefined ? (
           '0'
-        )}
+        ) : null}
         <span className="w-20 text-xs text-center">{text}</span>
       </Link>
     </li>
--- client/src/utils/validate.js
+++ client/src/utils/validate.js
@@ -1,49 +1,92 @@
 const validate = {
+  // 회원가입, (마이페이지 비밀번호변경 부분 포함)
   email: {
     required: '아이디(이메일)를 입력해주세요',
     pattern: {
-      value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
-      message: '이메일 형식이 아닙니다.',
+      value: /^[A-Za-z0-9.\-_]+@([A-Za-z0-9-]+\.)+[A-Za-z]{2,}$/i,
+      message: '올바른 이메일 형식이 아닙니다.',
     },
   },
   password: {
     required: '비밀번호를 입력해주세요',
     pattern: {
-      value: /^(?=.*[A-Za-z\d])(?=.*\d)(?=.*[!@#^*_])[A-Za-z\d!@#^*_]{8,}$/i,
+      value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#^*_])[A-Za-z\d!@#^*_]{8,}$/i,
       message:
-        '비밀번호는 8자 이상, 대소문자 구분, 특수기호 1개 이상 사용해주세요. (사용가능한 특수문자 : !@#^*_)',
+        '비밀번호는 8자 이상, 특수기호 1개 이상 포함해야합니다. (사용가능한 특수문자 : !@#^*_)',
     },
   },
-  passwordCheck: {
-    required: '비밀번호를 확인해주세요',
-    pattern: {
-      value: /^(?=.*[A-Za-z\d])(?=.*\d)(?=.*[!@#^*_])[A-Za-z\d!@#^*_]{8,}$/i,
-      message: '입력된 비밀번호와 일치하지 않습니다.',
-    },
-  },
+  // passwordCheck: {
+  //   required: '비밀번호를 확인해주세요',
+  //   pattern: {
+  //     value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#^*_])[A-Za-z\d!@#^*_]{8,}$/i,
+  //     message: '입력된 비밀번호와 일치하지 않습니다.',
+  //   },
+  // },
   displayName: {
     required: '이름(닉네임)을 입력해주세요',
     pattern: {
-      value: /^[가-힣A-Za-z\d_-]{6,}$/i,
+      value: /^[a-zA-Zㄱ-힣0-9_]{2,8}$/i,
       message:
-        "한글/영문(대소문자)/숫자 6자 이상이여야 합니다. (특수문자는  '-' , '_' 만 허용)",
+        '닉네임은 최소 2자 이상, 8자 이하로 한글/영문(대소문자)/숫자/특수문자를 포함해야합니다. (사용가능한 특수문자 : _)',
     },
   },
+  // 헬스장, (커뮤니티 제목/내용 부분 포함)
   tag: {
     required: '태그를 선택해주세요',
-    pattern: {
-      value: /^[a-z\s\d-]{2,}$/i,
-      message:
-        "태그 이름은 영소문자 10자 이하여야 합니다. (특수문자는 '-'만 허용)",
-    },
   },
   title: {
-    required: '태그를 입력해주세요',
+    required: '제목을 입력해주세요',
+  },
+  contents: {
+    required: '내용을 입력해주세요',
+  },
+  phoneNumber: {
+    required: '핸드폰 번호를 입력해주세요',
     pattern: {
-      value: /^[a-z\s\d-]{2,}$/i,
-      message:
-        "태그 이름은 영소문자 10자 이하여야 합니다. (특수문자는 '-'만 허용)",
+      value: /^\d{2,3}-\d{3,4}-\d{4}$/i,
+      message: '숫자만 입력 가능합니다. (사용가능한 특수문자 : -)',
     },
+  },
+  review: {
+    required: '리뷰를 작성해주세요',
+    pattern: {
+      value: /^[a-zA-Zㄱ-힣0-9_]{10,}$/i,
+      message: '리뷰는 최소 10자 이상으로 작성해야합니다.',
+    },
+  },
+  gymName: {
+    required: '헬스장 상호명을 입력해주세요',
+    pattern: {
+      value: /^[a-zA-Zㄱ-힣0-9_]{2,}$/i,
+      message: '상호명은 최소 2자 이상으로 작성해야합니다.',
+    },
+  },
+  gymImg: {
+    required: '이미지를 등록해주세요',
+    pattern: {
+      value: /(.*?)\.(jpg|jpeg|png|gif|bmp)$/i,
+      message: '올바른 이미지 형식이 아닙니다.',
+    },
+  },
+  mainPrice: {
+    required: '대표 가격을 입력해주세요',
+    pattern: {
+      value: /^[0-9]+$/i,
+      message: '숫자만 입력이 가능합니다.',
+    },
+  },
+  detailPrice: {
+    required: '상세 가격을 입력해주세요',
+    pattern: {
+      value: /^[0-9]+$/i,
+      message: '숫자만 입력이 가능합니다.',
+    },
+  },
+  gymOpenTime: {
+    required: '운영 시간을 입력해주세요.',
+  },
+  gymAddress: {
+    required: '주소를 입력해주세요',
   },
 };
 
