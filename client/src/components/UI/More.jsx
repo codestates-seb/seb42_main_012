218fed83baa33ca987d0b21da5707ae55901ffe0
kimyonghui <kyhui1115@gmail.com>
2023-03-29 21:21:56 +0900

    fix: 오류수정

--- client/src/components/UI/More.jsx
+++ client/src/components/UI/More.jsx
@@ -5,10 +5,11 @@ function More() {
   const param = useParams();
   const location = useLocation();
   const path = location.pathname;
+  console.log(path);
 
   return (
     <>
-      {path === `/board/${param.id}` ? (
+      {/* {path === `/board/${param.id}` ? (
         <div className="absolute top-20 right-4 z-10 bg-[#fff] border border-[var(--main)] rounded-lg text-center flex flex-col justify-center items-center">
           <BasicButton page="more" text="수정하기" />
           <BasicButton
@@ -19,18 +20,24 @@ function More() {
             id={param.id}
           />
         </div>
-      ) : (
-        <div className="absolute top-20 right-4 z-10 bg-[#fff] border border-[var(--main)] rounded-lg text-center flex flex-col justify-center items-center">
-          <BasicButton page="more" text="수정하기" nav="/gyms/gymedit" />
-          <BasicButton
-            page="more"
-            text="삭제하기"
-            nav="/gyms"
-            path={path}
-            id={param.id}
-          />
-        </div>
-      )}
+      ) : ( */}
+      <div className="absolute top-20 right-4 z-10 bg-[#fff] border border-[var(--main)] rounded-lg text-center flex flex-col justify-center items-center">
+        <BasicButton
+          page="more"
+          text="수정하기"
+          nav="/gyms/gymedit"
+          path={path}
+          id={param.id}
+        />
+        <BasicButton
+          page="more"
+          text="삭제하기"
+          nav="/gyms"
+          path={path}
+          id={param.id}
+        />
+      </div>
+      {/* )} */}
     </>
   );
 }
