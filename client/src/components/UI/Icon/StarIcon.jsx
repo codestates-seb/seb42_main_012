9267080ba6f28d2afe66754fe97e399a5189721d
kimyonghui <kyhui1115@gmail.com>
2023-04-05 22:09:28 +0900

    rename: gitignore 수정

---
+++ client/src/components/UI/Icon/StarIcon.jsx
@@ -0,0 +1,11 @@
+import { HiStar } from 'react-icons/hi';
+
+function StarIcon() {
+  return (
+    <div className="text-[var(--main)]">
+      <HiStar />
+    </div>
+  );
+}
+
+export default StarIcon;
